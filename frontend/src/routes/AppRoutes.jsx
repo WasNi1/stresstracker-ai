f6f2dd3742625e232767f568f0205440b26be510
WasNi1 <rajabanifawwas@gmail.com>
2026-05-28 11:12:17 +0700

    Menambah routingan ke dashboardrekomendasi dan riwayatrekomendasi

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,10 +3,10 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import ForgotPassword from '../pages/ForgotPassword'
 import Dashboard from '../pages/Dashboard'
-import DashboardRingkasanAI from '../pages/DashboardRingkasanAI'
+import DashboardRekomendasi from '../pages/DashboardRekomendasi'
 import DailyInput from '../pages/DailyInput'
 import Riwayat from '../pages/Riwayat'
-import RiwayatRingkasanAI from '../pages/RiwayatRingkasanAI'
+import RiwayatRekomendasi from '../pages/RiwayatRekomendasi'
 import Akun from '../pages/Akun'
 
 function AppRoutes() {
@@ -14,13 +14,13 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard />} />
-        <Route path='/dashboard-ai' element={<DashboardRingkasanAI />} />
+        <Route path='/dashboard-rekomendasi' element={<DashboardRekomendasi />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/input-harian' element={<DailyInput />} />
         <Route path='/riwayat' element={<Riwayat />} />
-        <Route path='/riwayat-ai' element={<RiwayatRingkasanAI />} />
+        <Route path='/riwayat-rekomendasi' element={<RiwayatRekomendasi />} />
         <Route path='/akun' element={<Akun />} />
       </Routes>
     </BrowserRouter>
