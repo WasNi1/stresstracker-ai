42363d626449faaac155065db4dfcb70cb1aeef3
WasNi1 <rajabanifawwas@gmail.com>
2026-05-28 10:03:23 +0700

    Menambahkan route ke DashboardRingkasanAi dan RiwayatRingkasanAI Pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,8 +3,10 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import ForgotPassword from '../pages/ForgotPassword'
 import Dashboard from '../pages/Dashboard'
+import DashboardRingkasanAI from '../pages/DashboardRingkasanAI'
 import DailyInput from '../pages/DailyInput'
 import Riwayat from '../pages/Riwayat'
+import RiwayatRingkasanAI from '../pages/RiwayatRingkasanAI'
 import Akun from '../pages/Akun'
 
 function AppRoutes() {
@@ -12,11 +14,13 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard-ai' element={<DashboardRingkasanAI />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/input-harian' element={<DailyInput />} />
         <Route path='/riwayat' element={<Riwayat />} />
+        <Route path='/riwayat-ai' element={<RiwayatRingkasanAI />} />
         <Route path='/akun' element={<Akun />} />
       </Routes>
     </BrowserRouter>
